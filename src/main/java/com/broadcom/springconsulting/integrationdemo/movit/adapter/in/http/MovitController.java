bd7709c64f727f1cfdd36a3540981a5c495e4a3a
Daniel Frey <dmfrey@gmail.com>
2025-10-28 17:02:13 -0400

    remove public accessor

--- src/main/java/com/broadcom/springconsulting/integrationdemo/movit/adapter/in/http/MovitController.java
+++ src/main/java/com/broadcom/springconsulting/integrationdemo/movit/adapter/in/http/MovitController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping( "/movit" )
-public class MovitController {
+class MovitController {
 
     private final PrepareRequestDownloadUseCase prepareRequestDownloadUseCase;
     private final SendProgramUseCase sendProgramUseCase;
